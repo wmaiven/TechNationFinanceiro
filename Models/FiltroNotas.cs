e94403993eb9499148adda2734e6a354c631f944
wmaiven <maivencunha@hotmail.com>
2024-07-05 17:59:25 -0300

    tirando aviso de status

--- Models/FiltroNotas.cs
+++ Models/FiltroNotas.cs
@@ -5,6 +5,6 @@
         public int MesEmissao { get; set; }
         public int MesCobranca { get; set; }
         public int MesPagamento { get; set; }
-        public string Status { get; set; }
+        public string? Status { get; set; }
     }
 }
