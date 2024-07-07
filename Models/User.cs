562b774cfb1637657e589ad1b34667ffde4870a8
wmaiven <maivencunha@hotmail.com>
2024-07-07 20:28:20 -0300

    fazendo com q id n seja required ja q ele se autoincrementa no banco

--- Models/User.cs
+++ Models/User.cs
@@ -2,7 +2,7 @@
 {
     public class User
     {
-        public required int Id { get; set; }
+        public int Id { get; set; }
         public required string Username { get; set; }
         public required string Password { get; set; } 
     }
